0a5879c90c6ccd86b815d84f8ff17670611cfcd3
Joe Biggins <josephjbiggins@icloud.com>
2020-07-22 15:37:46 -0500

    add additional functions to omaciel fauxfactory repo to extend functionality

--- setup.py
+++ setup.py
@@ -13,18 +13,18 @@ def read(*paths):
         return handle.read()
 
 
-LONG_DESCRIPTION = (read('README.rst') + '\n\n' +
+LONG_DESCRIPTION = (read('README.md') + '\n\n' +
                     read('AUTHORS.rst') + '\n\n' +
                     read('HISTORY.rst'))
 
 setup(
     name='fauxfactory',
     description='Generates random data for your tests.',
-    long_description=LONG_DESCRIPTION,
+    long_description='Test Data Generator',
     use_scm_version=True,
-    author='Og Maciel',
-    author_email='omaciel@ogmaciel.com',
-    url='https://github.com/omaciel/fauxfactory',
+    author='Joe Biggins',
+    author_email='jjbiggins@joebiggins.io',
+    url='https://github.com/jjbiggins/fauxfactory',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     include_package_data=True,
     keywords='python automation data',
@@ -33,7 +33,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
